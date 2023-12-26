9c90aa0f2b3e7d63899537fca16217aa8089284e
Dmitry Buevich <buevichd@gmail.com>
2023-12-26 18:02:13 +0300

    Lection 24, Django admin

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
@@ -13,6 +14,11 @@ class Question(models.Model):
     pub_date = models.DateTimeField(verbose_name='Publication date')
     status = models.CharField(max_length=2, choices=Status, default=Status.NEW)
 
+    @admin.display(
+        boolean=True,
+        description='Published recently?',
+        ordering='pub_date'
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - timezone.timedelta(days=1) <= self.pub_date <= now
@@ -26,6 +32,10 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, related_name='choices',
                                  on_delete=models.CASCADE)
+
+    @admin.display(ordering='question__question_text', description='Question Text')
+    def get_question_text(self):
+        return self.question.question_text
 
     def __str__(self):
         return f'{self.question.question_text} - {self.choice_text}'
