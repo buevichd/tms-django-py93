4325432469371925ece586b5a178e0038b028af1
Dmitry Buevich <buevichd@gmail.com>
2024-01-09 17:55:34 +0300

    Lection 26, base_template_context_processor

---
+++ mysite/context_processors.py
@@ -0,0 +1,16 @@
+from django.http import HttpRequest
+
+
+def base_template_context_processor(request: HttpRequest):
+    if 'next' not in request.GET:
+        return {}
+    next = request.GET['next']
+    if next.startswith('/polls'):
+        base_template = 'polls/base.html'
+    elif next.startswith('/articles'):
+        base_template = 'articles/base.html'
+    elif next.startswith('/shop'):
+        base_template = 'shop/base.html'
+    else:
+        base_template = None
+    return {'base_template': base_template}
