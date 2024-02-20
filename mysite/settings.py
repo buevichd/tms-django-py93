8d22015123e175df6d21fd2cd9af1a1ba517f738
Dmitry Buevich <buevichd@gmail.com>
2024-02-20 21:07:44 +0300

    Lection 38

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,15 +81,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_py93',
-        'USER': 'django_py93',
-        'PASSWORD': 'django_py93',
-        'HOST': 'localhost',
-        'PORT': 5432,
+if os.getenv('CI') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'django_py93',
+            'USER': 'django_py93',
+            'PASSWORD': 'django_py93',
+            'HOST': 'localhost',
+            'PORT': 5432,
+        }
 }
 
 # Password validation
